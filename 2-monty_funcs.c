80b93cd04d8ce92976939ebf418f38942fe082df
Kagendoooo <kagendoooo@gmail.com>
2023-09-22 02:45:20 +0300

    Added pchar and pstr

--- 2-monty_funcs.c
+++ 2-monty_funcs.c
@@ -3,9 +3,51 @@
  * monty_nop - Does absolutely nothing for 'nop' opcode
  * @stack: Pointer to top node to linked list.
  * @line_number: current working line number
-*/
+ */
 void monty_nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
+	(void)line_number;
+}
+
+/**
+ * monty_pchar - Prints the character in the top value
+ * @stack: Pointer to top node of linked list
+ * @line_number: The current working line number
+*/
+void monty_pchar(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL)
+	{
+		set_op_tok_error(pchar_error(line_number, "stack empty"));
+		return;
+	}
+	if ((*stack)->next->n < 0 || (*stack)->next->n > 127)
+	{
+		set_op_tok_error(pchar_error(line_number,
+					"value out of range"));
+		return;
+	}
+
+	printf("%c\n", (*stack)->next->n);
+}
+
+/**
+ * monty_pstr - Prints the string contained in linked list
+ * @stack: Pointer to the top node of linked list
+ * @line_number: The current working line number
+*/
+void monty_pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = (*stack)->next;
+
+	while (temp && temp->n != 0 && (temp->n > 0 && temp->n <= 127))
+	{
+		printf("%c", temp->n);
+		temp = temp->next;
+	}
+
+	printf("\n");
+
 	(void)line_number;
 }
