8ff92634ccc47409fc0424393c84d87f1d875725
Kamundia-Simon <simonkamundia8@gmail.com>
2023-09-22 00:51:50 +0300

    stack & div error

--- exe_monty.c
+++ exe_monty.c
@@ -8,35 +8,35 @@
  */
 void (*get_op_func(char *opcode))(stack_t**, unsigned int)
 {
-        instruction_t op_funcs[] = {
+	instruction_t op_funcs[] = {
 		{"pall", monty_pall},
 		{"pint", monty_pint},
-                {"push", monty_push},
-                {"pop", monty_pop},
-                {"swap", monty_swap},
-                {"add", monty_add},
-                {"nop", monty_nop},
-                {"sub", monty_sub},
-                {"div", monty_div},
-                {"mul", monty_mul},
-                {"mod", monty_mod},
+		{"push", monty_push},
+		{"pop", monty_pop},
+		{"swap", monty_swap},
+		{"add", monty_add},
+		{"nop", monty_nop},
+		{"sub", monty_sub},
+		{"div", monty_div},
+		{"mul", monty_mul},
+		{"mod", monty_mod},
 		{"queue", monty_queue},
-                {"pchar", monty_pchar},
-                {"pstr", monty_pstr},
-                {"rotl", monty_rotl},
-                {"rotr", monty_rotr},
-                {"stack", monty_stack},
-                {NULL, NULL}
-        };
-        int i;
+		{"pchar", monty_pchar},
+		{"pstr", monty_pstr},
+		{"rotl", monty_rotl},
+		{"rotr", monty_rotr},
+		{"stack", monty_stack},
+		{NULL, NULL}
+	};
+	int i;
 
-        for (i = 0; op_funcs[i].opcode; i++)
-        {
-                if (strcmp(opcode, op_funcs[i].opcode) == 0)
-                        return (op_funcs[i].f);
-        }
+	for (i = 0; op_funcs[i].opcode; i++)
+	{
+		if (strcmp(opcode, op_funcs[i].opcode) == 0)
+			return (op_funcs[i].f);
+	}
 
-        return (NULL);
+	return (NULL);
 }
 
 /**
@@ -47,60 +47,77 @@ void (*get_op_func(char *opcode))(stack_t**, unsigned int)
  */
 int exe_monty(FILE *script_fd)
 {
-        stack_t *stack = NULL;
-        char *line = NULL;
-        size_t len = 0, exit_status = EXIT_SUCCESS;
-        unsigned int line_number = 0, prev_tok_len = 0;
-        void (*op_func)(stack_t**, unsigned int);
+	stack_t *stack = NULL;
+	char *line = NULL;
+	size_t len = 0, exit_status = EXIT_SUCCESS;
+	unsigned int line_number = 0, prev_tok_len = 0;
+	void (*op_func)(stack_t**, unsigned int);
 
-        if (init_stack(&stack) == EXIT_FAILURE)
-                return (EXIT_FAILURE);
+	if (init_stack(&stack) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
 
-        while (getline(&line, &len, script_fd) != -1)
-        {
-                line_number++;
-                op_toks = strtow(line, DELIMS);
-                if (op_toks == NULL)
-                {
-                        if (is_empty_line(line, DELIMS))
-                                continue;
-                        free_stack(&stack);
-                        return (malloc_error());
-                }
-                else if (op_toks[0][0] == '#') /* comment line */
-                {
-                        free_tokens();
-                        continue;
-                }
-                op_func = get_op_func(op_toks[0]);
-                if (op_func == NULL)
-                {
-                        free_stack(&stack);
-                        exit_status = unknown_op_error(op_toks[0], line_number);
-                        free_tokens();
-                        break;
-                }
-                prev_tok_len = token_arr_len();
-                op_func(&stack, line_number);
-                if (token_arr_len() != prev_tok_len)
-                {
-                        if (op_toks && op_toks[prev_tok_len])
-                                exit_status = atoi(op_toks[prev_tok_len]);
-                        else
-                                exit_status = EXIT_FAILURE;
-                        free_tokens();
-                        break;
-                }
-                free_tokens();
-        }
-        free_stack(&stack);
+	while (getline(&line, &len, script_fd) != -1)
+	{
+		line_number++;
+		op_toks = strtow(line, DELIMS);
+		if (op_toks == NULL)
+		{
+			if (is_empty_line(line, DELIMS))
+				continue;
+			free_stack(&stack);
+			return (malloc_error());
+		}
+		else if (op_toks[0][0] == '#') /* comment line */
+		{
+			free_tokens();
+			continue;
+		}
+		op_func = get_op_func(op_toks[0]);
+		if (op_func == NULL)
+		{
+			free_stack(&stack);
+			exit_status = unknown_op_error(op_toks[0], line_number);
+			free_tokens();
+			break;
+		}
+		prev_tok_len = token_arr_len();
+		op_func(&stack, line_number);
+		if (token_arr_len() != prev_tok_len)
+		{
+			if (op_toks && op_toks[prev_tok_len])
+				exit_status = atoi(op_toks[prev_tok_len]);
+			else
+				exit_status = EXIT_FAILURE;
+			free_tokens();
+			break;
+		}
+		free_tokens();
+	}
+	free_stack(&stack);
 
-        if (line && *line == 0)
-        {
-                free(line);
-                return (malloc_error());
-        }
+	if (line && *line == 0)
+	{
+		free(line);
+		return (malloc_error());
+	}
 
-        free(line);
-        return (exit_status);
+	free(line);
+	return (exit_status);
 }
+
+/**
+ * free_tokens - Frees the global op_toks array of str
+ */
+void free_tokens(void)
+{
+	size_t i = 0;
+
+	if (op_toks == NULL)
+		return;
+
+	for (i = 0; op_toks[i]; i++)
+		free(op_toks[i]);
+
+	free(op_toks);
+}
+
