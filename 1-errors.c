2ccb79af34c1abacf03e043db588e648f8984429
Kamundia-Simon <simonkamundia8@gmail.com>
2023-09-22 02:56:57 +0300

    pchar_error pint_error

--- 1-errors.c
+++ 1-errors.c
@@ -33,3 +33,27 @@ int pop_error(unsigned int line_number)
 	fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 	return (EXIT_FAILURE);
 }
+
+/**
+ * pchar_error - Prints pchar error messages for empty stacks
+ * @line_number: Line number in Monty
+ * @msg: corresponding error message to print
+ * Return: EXIT_FAILURE
+ */
+
+int pchar_error(unsigned int line_number, char *msg)
+{
+	fprintf(stderr, "L%u: can't pchar, %s\n", line_number, msg);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * pint_error - Prints pint error msg for empty stacks.
+ * @line_number: Line number in Monty
+ * Return: EXIT_FAILURE
+ */
+int pint_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+	return (EXIT_FAILURE);
+}
