df30bba7b37051e8fe1c0cd4de479acb73ab31d0
Kamundia-Simon <simonkamundia8@gmail.com>
2023-09-22 04:32:56 +0300

    added monty_add

--- monty.h
+++ monty.h
@@ -54,6 +54,7 @@ void monty_push(stack_t **stack, unsigned int line_number);
 void monty_pall(stack_t **stack, unsigned int line_number);
 void monty_pop(stack_t **stack, unsigned int line_number);
 void monty_swap(stack_t **stack, unsigned int line_number);
+void monty_add(stack_t **stack, unsigned int line_number);
 void monty_sub(stack_t **stack, unsigned int line_number);
 void monty_div(stack_t **stack, unsigned int line_number);
 void monty_mul(stack_t **stack, unsigned int line_number);
